0d22e5fffca9d9b8bf507ddb41c0d8a6e2fd5918
Nikita <monolit9677@mail.ru>
2022-06-15 17:29:45 +0700

    Домашнеезадание2

---
+++ HomeWorkSeminar2a/Program.cs
@@ -0,0 +1,47 @@
+﻿
+/*Задача10
+int CutNumber()
+
+{
+  int num = new Random().Next(100,1000);
+  Console.WriteLine($"Current number is {num}");
+
+  int sot = (num / 10) % 10;
+  
+  int result = sot;
+  return result;
+}
+Console.WriteLine(CutNumber());
+*/
+
+/*Задача13
+Console.WriteLine("Input integer number");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine (" third digit is " + anyNumberText[2]);
+}
+else 
+{
+Console.WriteLine (" third digit is not exist ");
+}
+*/
+
+/*Задача15
+Console.WriteLine("Input integer number one to seven");
+int DayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int DayNumber) 
+{
+
+if (DayNumber == 6 || DayNumber == 7)
+{
+    Console.WriteLine("This is a weekend");
+}
+else 
+if (DayNumber >= 1 && DayNumber <= 5)
+Console.WriteLine("This is work day");
+}
+CheckingTheDayOfTheWeek(DayNumber);
+*/
