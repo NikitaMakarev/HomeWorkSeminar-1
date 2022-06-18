3a4cf23e2acb7942a4d5e6ce0f4334e27c4b7669
Nikita <monolit9677@mail.ru>
2022-06-18 16:28:55 +0700

    Семинар3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int FindQuart(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    
+    return -1;
+}
+
+int result = FindQuart(2,4);
+
+if(result == -1) Console.WritLeline("Данная точка расположена на осях");
+else Console.WritLeline($"Точка находится в (result) четверти");
