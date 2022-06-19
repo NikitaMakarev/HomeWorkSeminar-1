52d820bcedeec6b97de5765a953626da191aa482
Nikita <monolit9677@mail.ru>
2022-06-19 20:38:02 +0700

    HomeWork4

---
+++ HomeWorkSeminar4/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача29
+int size = 8;
+int [] array =new int [size]; 
+
+for(int i = 0; i < size; i++)
+{
+   array [i]= Convert.ToInt32(Console.ReadLine());
+}
+
+for(int i =0; i < size; i++)
+{
+    Console.Write(array[i] + ", ");
+}
+*/
+
+/*Задача25
+int Exponentiation(int A, int B)
+{
+    int result = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число А: ");
+int A =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B =Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(A, B);
+  Console.WriteLine("Ответ: " + exponentiation);
+*/
+
