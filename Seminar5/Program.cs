2c0fe47beda17cb758759cff90c6f2dbca9caea9
Nikita <monolit9677@mail.ru>
2022-06-22 18:27:15 +0700

    seminar5

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,5 +1,5 @@
 ﻿// шаблон 
-
+/*Задача1
 int[] CreateRandomArray(int size, int min, int max)
 {
     int [] newArray = new int [size];
@@ -38,3 +38,59 @@ int [] myArray = CreateRandomArray(12, -9, 9); // дома более подро
 
 Console.WriteLine("Sum of positive numbers is " + FindPositiveSum(myArray));
 Console.WriteLine("Sum of negative numbers is " + FindNegativeSum(myArray));
+*/
+
+/*Задача2
+int[] ReverseElementsArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+      array[i] *= -1;
+    }
+    return array;
+}
+*/
+
+/*Задача3
+bool FindNumberInArray(int[] array, int aa)
+{
+  bool bb = false;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] == aa) return true;
+  }
+  return false;
+} 
+*/
+
+/*Задача4
+int FindCountElemensInRenge(int[] array, int min, int max)
+{
+  int count = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > min && array[i] < max) count++;
+     }
+      return count;
+}
+string OutputArrayConsole(int [] array)
+{
+  string ss = "";
+  for(int i = 0; i < array.Length; i++)
+  {
+    ss + = array[i] + ",";
+  }
+  return ss;
+}
+*/
+/*
+//Задача2
+Console.WriteLine("reverse array " + OutputArrayConsole(ReverseElementsArray(myArray)));
+//Задача3
+int find = 16;
+Console.WriteLine("В массиве число " + find + ((FindNumberInArray(myArray,find)) ? "присутсвует" : "не присутсвует"));
+//Задача4
+int minnum = 10;
+int maxnum = 99;
+Console.WriteLine("Количество цифр в отрезке " + minnum + "до" + "=" + FindCountElemensInRenge(myArray, minnum, maxnum));    
+*/
