092bb15e194e50d847e2c911999c24908f4c66d8
Nikita <monolit9677@mail.ru>
2022-06-30 14:24:42 +0700

    Seminar7

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -24,33 +24,54 @@ int [,] CreateArrayIJ(int a, int b)
     int [,] newMatrix = new int[a, b];
 
     for(int i = 0; i < a; i++)
-    {
         for(int j = 0; j < b; j++)
+           newMatrix[i, j] = i + j;     
+    
+    return newMatrix;
+}
+void ShowTwoDimArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
         {
-           newMatrix[i, j] = i + j;
            Console.Write(newMatrix[i, j] + " ");      
         }
         Console.WriteLine();
     }
-    return newMatrix;
 }
 
-int[,] array = CreateArrayIJ(4, 5);
+int[,] array = CreateRandomTwoDemArray(4, 5);
 */
-/*
-int [,] CreateArrayEvenIndexQuad(int a, int b)
+/*Задача3
+int [,] UpdateArrayEvenIndexQuad(int [,] array)
 {
-    for(int i = 0; i < a; i++)
-    {
-        for(int j = 0; j < b; j++)
-        {
-           if (i ==j)   
-        }
-        Console.WriteLine();
-    }
-    return newMatrix;
-}
-int [,] myArray = CreateArrayEvenIndexQuad(4,5)
+    int step = 2;
+    for(int i = 0; i < array.GetLength(0); i+= step)
+        for(int j = 0; j < array.GetLength(1); j+= step)
+            array[i,j] *= array[i,j];
 
-int[,] array = CreateArrayIJ(4, 5, 2, 15);
+    return array;
+}
+
+int [,] myArray = CreateRandomTwoDemArray(4, 5, 2, 15);
+
+Console.WriteLine();
+
+CreateArrayEvenIndexQuad(myArray);
+*/
+
+/*Задача4
+int [,] SumOfDiagonal(int [,] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(1); i++)
+        sum += array[i,i];
+
+    return sum;
+}
+
+int [,] myArray = CreateRandomTwoDemArray(7, 2, 15);
+
+Console.WriteLine(SumOfDiagonal(myArray));
 */
