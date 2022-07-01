5c6464ad9000bce78e8c28943d074029b1b04960
Nikita <monolit9677@mail.ru>
2022-07-01 14:50:23 +0700

    HomeWork7

--- HomeWorkSeminar7/Program.cs
+++ HomeWorkSeminar7/Program.cs
@@ -56,14 +56,63 @@ Console.WriteLine("Enter the array's number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the array's number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] array_result = new int[m,n];
-PrintTwoDimentsionalArray(array_result);
-FindArrayColumnMean(array_result);
+int[,] arrayresult = new int[m,n];
+PrintTwoDimentsionalArray(arrayresult);
+FindArrayColumnMean(arrayresult);
 */
 
+/*Задача50
+int[,] PrintTwoDimentsionalArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 100);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+void FindElementInArray(int[,] matrix, int a, int b)
+{
+    int element = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == a && j == b)
+            {
+                element = matrix[i, j];
+               
+            }
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Элемент с индексом ({a}, {b}) является {element}.");
+}
+Console.WriteLine("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите номер строки элемента: ");
+int rowind = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите номер столбца элемента: ");
+int colind = Convert.ToInt32(Console.ReadLine());
 
-
-
+int[,] arrayresult = new int[m, n];
+PrintTwoDimentsionalArray(arrayresult);
+if (rowind < arrayresult.GetLength(0) && colind < arrayresult.GetLength(1))
+{
+    FindElementInArray(arrayresult, rowind, colind);
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine($"Элемент с индексом ({rowind}, {colind}) отсутствует в массиве. ");
+}
+*/
