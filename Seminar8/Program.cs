475c67e4d4c93f979985c4d8c9b4335bfadbec17
Nikita <monolit9677@mail.ru>
2022-07-02 15:41:30 +0700

    seminar8

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -41,12 +41,12 @@ myArray = ChangeLines(myArray, 0, myArray.GetLength(0) -1);
 Console.WriteLine();
 ShowArray(myArray);
 */
-
+/*Задача3
 int [,] ChangeRowsColumns(int[,] array)
 {
-    for(int i = 0; i < array.GetLenght(0); i++)
+    for(int i = 0; i < array.GetLength(0) - 1; i++)
     {
-        for( int j = 0; j < array.GetLength(1); j++)
+        for( int j = i + 1; j < array.GetLength(1); j++)
         {
             int temp = array[i, j];
             array[i, j] = array[j, i];
@@ -56,8 +56,53 @@ int [,] ChangeRowsColumns(int[,] array)
     return array;
 }
 
-int [,] myArray = CrateTwoDimArray(4, 4, 1, 9);
+int m, n, min, max;
+do
+{
+    Console.WriteLine("Введите данные.Число строк должно быть равно числу столбцов: ");
+    m = Convert.ToInt32(Console.ReadLine());
+    n = Convert.ToInt32(Console.ReadLine());
+    min = Convert.ToInt32(Console.ReadLine());
+    max = Convert.ToInt32(Console.ReadLine());
+}
+
+while(m != n);
+int [,] myArray = CrateTwoDimArray(m, n, min, max);
 ShowArray(myArray);
 Console.WriteLine();
 myArray = ChangeRowsColumns(myArray);
 ShowArray(myArray);
+*/
+
+/*Задача3
+int[,] DeleteRowColumn(int[,] array)
+{
+    int minRow = 0;
+    int minColumn = 0;
+    int min = array[minRow, minColumn];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for( int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array [i, j] < min) 
+            {
+              min = array[i, j];
+              minRow = i;
+              minColumn = j;
+            }  
+        }
+    }
+
+   for(int i = 0; i < array.GetLength(0); i++)
+      array[i, minColumn] = 0;
+   for( int j = 0; j < array.GetLength(1); j++)
+     array[minRow, j] = 0;
+   return array;
+}
+
+int [,] myArray = CrateTwoDimArray(4, 6, 1, 9);
+ShowArray(myArray);
+Console.WriteLine();
+myArray = DeleteRowColumn(myArray);
+ShowArray(myArray);
+*/
