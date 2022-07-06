86e4133a27cd356991e6e6e1b645633281ee3942
Nikita <monolit9677@mail.ru>
2022-07-06 12:37:34 +0700

    HomeWork9

---
+++ HomeWorkSeminar9/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+int Sum(int m, int n)
+{
+    if(m != 1)
+      (m * Sum(n + 1, m)) / 2;
+
+    Console.Write(m + " ");
+}
+
+Console.WriteLine("Input integer positive number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input integer positive number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+Sum(m , n);
+*/
+
